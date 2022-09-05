d1783ec855dc8647a4afe2f44e0c337849e80cf1
Ali Husnain <pkihusnain220@gmail.com>
2022-09-05 09:26:55 -0700

    variable or

---
+++ function.js
@@ -0,0 +1,26 @@
+ //variable declaration ; 
+
+let fullName = "Ali Husnain"; //let variables can not re-declared the scope of a block or statement is limited, 
+
+fullName = "Husnain bahtti"; // update value 
+
+var lastName = "Husnain";
+// the var keyword,which declares a variable globally, or locally to an entire function regardless of block scope.
+
+const firstName  = "Ali"
+// const variables can neither be updated nor re-declared
+
+document.write(fullName + "</br>");
+
+document.write(lastName+ "</br>");
+
+document.write(firstName + "</br>");
+
+//Check ASCII code a character or Integer
+
+let string = prompt("Enter The character" );//enter a character
+
+let result = string.charCodeAt(0); //check the value character
+
+document.write(result); // print ASCII value
+
