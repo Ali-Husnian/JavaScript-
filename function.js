e0caae49080701a108922332f7719db650f49860
Ali Husnain <pkihusnain220@gmail.com>
2022-09-06 23:31:14 -0700

    add and modify Datatype

--- function.js
+++ function.js
@@ -29,7 +29,7 @@ document.write(result); // print ASCII value
 */
 
 // Addtion to Number //////////////////////////////////////
-
+/*
 let firstNum = parseInt(prompt("Enter the 1st number"));
 
 let secondNum =  parseInt(prompt("Enter the 2nd number"));
@@ -37,7 +37,35 @@ let secondNum =  parseInt(prompt("Enter the 2nd number"));
 resultNum = firstNum + secondNum ;
 
 document.write(resultNum);
+*/
 
+///////////////////////DataType////////////////////////////
+
+var int , char , float , _NaN , object , array, undefine;
+
+int = 5;
+document.write(typeof(int) + "</br>");
+
+char = "Husnain";
+document.write(typeof(char) + "</br>");
+
+float = 23.44;
+document.write(typeof(float) + "</br>");
+
+
+document.write(typeof(undefine) + "</br>");
+
+array = [1,4,'dks']; 
+document.write(typeof(array) + "</br>");
+
+object = {
+    car: 'ooo',
+    call: 'phone'
+}
+document.write(typeof(object) + "</br>");
+_NaN = 10;
+
+var result = document.write(isNaN(_NaN) + "</br>");
 
 
 
