9594ba417053887d04e1ffd1d441b602f440e255
Ali Husnain <pkihusnain220@gmail.com>
2022-09-29 22:18:28 +0500

    modifi and add forEach loop and coninue statment

--- loops.js
+++ loops.js
@@ -6,7 +6,6 @@ for (let i= 1; i <= 10; i++) {
 document.write("</ul>");
 
 //do wlile
-
 let j = 1;
 do {
 document.write("Ali <br>");
@@ -15,7 +14,6 @@ j++;
 while (j <= 10);
 
 // while loop
-
 const arr = ["red","green","yellow","blue","gray"];
 let arrLen = arr.length;
 console.warn(arrLen);
@@ -26,4 +24,30 @@ while (index < arrLen) {
     index ++ ;
 }
 
+// forEach loop
+document.write("<br>")
+let car = [
+    "Honda",
+    "BMW",
+    "Suzoki",
+    'Hondi',
+    "OOOO",
+    "Marcdes"
+];
+car.forEach(myFuntion);
 
+function myFuntion(item , index , array ) {   // 1.item print   2.index print   3.print compleat array
+    document.write( index+". "+item +"<br>" + array);
+}
+
+// continue statment use in every loop
+document.write("<h1> continue statment <h1/>");
+
+for (let i = 0; i <= 10; i++) {
+
+    if( i == 5){
+        continue;  // skip 5 
+    }
+    document.write(i + "<br/>");
+    
+}
